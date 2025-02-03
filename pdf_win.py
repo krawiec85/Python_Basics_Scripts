4ad8cd447bfd009a6e2469a627cb45f5d952dd97
krawiec85 <krawiec85@o2.pl>
2025-02-03 10:27:44 +0100

    Init

--- pdf_win.py
+++ pdf_win.py
@@ -1,11 +1,17 @@
 import PyPDF2
 import os
+import uuid
 
+print("Rozpoczęcie procesu...")
 merger = PyPDF2.PdfMerger()
 
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
+        print(f"Dodawanie pliku: {file}")
         merger.append(file)
-merger.write("Razem.pdf")
+
+output_filename = "Razem.pdf"
+merger.write(output_filename)
 merger.close()
+print(f"Proces zakończony. Plik {output_filename} został utworzony.")
 
